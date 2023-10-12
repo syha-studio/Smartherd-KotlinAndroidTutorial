a6b914ff27d958a5f8690482c39b49e8bd568efe
syha-studio <syauqillah0908@gmail.com>
2023-10-12 10:25:04 +0700

    Section_2-2 Done

--- app/src/main/java/com/example/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/example/msgshareapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.msgshareapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var button : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.btnShowToast)
+        button.setOnClickListener(){
+            //Code
+            Log.i("MainActivity", "Button Was clicked !")
+
+            Toast.makeText(this, "Button was clicked !", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
